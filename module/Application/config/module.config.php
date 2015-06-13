7a19502ded1a5acedfa6b4e2a7561c8938622105
Stefano Valle <s.valle@mvassociati.it>
2015-06-13 11:50:07 +0200

    Move Webuser entity inside SharengoCore module

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -257,22 +257,6 @@ return array(
         ],
     ],
 
-    'doctrine' => array(
-        'driver' => array(
-            __NAMESPACE__ . '_driver' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
-            ),
-            'orm_default' => array(
-                'class'   => 'Doctrine\ORM\Mapping\Driver\DriverChain',
-                'drivers' => array(
-                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
-                )
-            ),
-        ),
-    ),
-
     // ACL
     'bjyauthorize' => array(
         'resource_providers' => [
