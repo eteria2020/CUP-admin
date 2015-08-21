ba83cfec88c985c2b9f291acdfc7e5f6609ee3c1
Stefano Valle <s.valle@mvlabs.it>
2015-08-21 14:21:59 +0200

    Remove comments

--- module/Application/src/Application/Form/TripCostFormFactory.php
+++ module/Application/src/Application/Form/TripCostFormFactory.php
@@ -17,17 +17,7 @@ class TripCostFormFactory implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        /*$entityManager = $serviceLocator->get('doctrine.entitymanager.orm_default');
-        $hydrator = new DoctrineHydrator($entityManager);
-        $I_clientService = $serviceLocator->get('SharengoCore\Service\CustomersService');
-        $countriesService = $serviceLocator->get('SharengoCore\Service\CountriesService');
-        $provincesService = $serviceLocator->get('SharengoCore\Service\ProvincesService');*/
-        $tripCostFieldset = new TripCostFieldset(
-            /*$I_clientService,
-            $countriesService,
-            $provincesService,
-            $hydrator*/
-        );
+        $tripCostFieldset = new TripCostFieldset();
 
         return new TripCostForm($tripCostFieldset);
     }
