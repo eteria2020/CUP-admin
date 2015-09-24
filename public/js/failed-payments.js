ded6fd365c7b7e57331342d1e8b7cffd9f6e431e
Francesco De Marchi <f.demarchi@mvlabs.it>
2015-09-24 15:20:16 +0200

    Added firstPaymentTryTs solution

--- public/js/failed-payments.js
+++ public/js/failed-payments.js
@@ -48,7 +48,7 @@ $(function() {
         },
         "order": [[0, 'desc']],
         "columns": [
-            {data: 'e.createdAt'},
+            {data: 'e.firstPaymentTryTs'},
             {data: 'cu.id'},
             {data: 'cu.name'},
             {data: 'cu.surname'},
