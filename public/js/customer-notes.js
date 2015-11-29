b642bcc72435506b1e5dccf75f2534e5b7986be8
Francesco <pinoy4@hotmail.com>
2015-11-29 15:34:39 +0100

    Ordered notes correctly

--- public/js/customer-notes.js
+++ public/js/customer-notes.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-    $('#notes-table').DataTable();
+	// Create the datatable and order it by descending date
+    var table = $('#notes-table').DataTable().order([0,"desc"]).draw();
     $('#new-note').html('');
 });
