afc5aaaac673e222f85dc92b1e5f2d18abd6bf2c
Massimo De Luisa <massimodeluisa@me.com>
2016-01-14 12:59:03 +0100

    1312 Various Fix (Created Reports Config File)

---
+++ config/autoload/reports.global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reports' => [
+        'nodeJsPath' => '/usr/lib/nodejs',
+        'uglifyJsBin' => realpath(__DIR__.'/../../').'/node_modules/uglify-js/bin/uglifyjs',
+    ],
+];
+
--- module/Reports/src/Reports/Service/ObfuscatorFactory.php
+++ module/Reports/src/Reports/Service/ObfuscatorFactory.php
@@ -10,16 +10,11 @@ class ObfuscatorFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        // Set the uglify path
-        $uglifybin = realpath(__DIR__.'/../../../../../').'/node_modules/uglify-js/bin/uglifyjs';
+        // Get paths
+        $nodeJsBinPath = realpath($serviceLocator->get('Configuration')['reports']['nodeJsBinPath']);
+        $uglifyJsPath = realpath($serviceLocator->get('Configuration')['reports']['uglifyJsPath']);
 
-        // Set the node.js path
-        $NodeJsOsXPath = '/usr/local/bin/node';
-        $NodeJsLinuxPath = '/usr/lib/nodejs';
-
-        $nodejsbin = file_exists($NodeJsLinuxPath) ? $NodeJsLinuxPath : $NodeJsOsXPath;
-
-        $module = new UglifyJs2Filter($uglifybin, $nodejsbin);
+        $module = new UglifyJs2Filter($uglifyJsPath, $nodeJsBinPath);
 
         // Set the params
         $module->setMangle(true);
