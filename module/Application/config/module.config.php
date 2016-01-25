ca16d479b7801b21ca4f8fa50ad628f65c5868ce
Massimo De Luisa <massimodeluisa@me.com>
2016-01-25 09:59:01 +0100

    1353 Fix reports route link.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1262,7 +1262,7 @@ return [
                 'icon'            => 'fa fa-line-chart',
                 'resource'        => 'superadmin',
                 'openOnNewWindow' => true,
-                'uri'             => 'reports',
+                'uri'             => '/reports',
             ],
             [
                 'label'           => 'Call center',
