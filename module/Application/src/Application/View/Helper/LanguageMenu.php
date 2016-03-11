024b3e98aa0bffcc2d56d309696460a59869282e
Andrea Cadorin <cadorin89@gmail.com>
2016-03-11 12:37:28 +0100

    add missing file

---
+++ module/Application/src/Application/View/Helper/LanguageMenu.php
@@ -0,0 +1,20 @@
+<?php
+namespace Application\View\Helper;
+
+
+class LanguageMenu
+{
+    private $languages;
+
+    private $currentLanguageLabel;
+
+    public function addLanguage($lang)
+    {
+        $laguages[] = $lang;
+    }
+
+    public function getCurrentLanguage()
+    {
+
+    }
+}
