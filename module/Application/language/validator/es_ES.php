54e2c0176b98a899ad2fda5d8fd447bffe329746
Stefano Valle <s.valle@mvassociati.it>
2016-05-26 15:33:53 +0200

    Spanish translations

--- module/Application/language/validator/es_ES.php
+++ module/Application/language/validator/es_ES.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    "Questo codice esiste già" => "",
-    "Esiste già un utente con lo stesso indirizzo email" => "",
-    "Esiste già un utente con lo stesso codice fiscale" => "",
-    "E' necessario essere maggiorenni" => "",
-    "E' necessario avere la patente da almeno un anno" => "",
-    "La partita IVA non può contenere spazi" => "",
-    "La partita IVA deve essere numerica, al più preceduta dal prefisso IT" => "",
-    "La partita IVA deve essere di 11 cifre, al più preceduta dal prefisso IT" => ""
+    "Questo codice esiste già" => "este codigo  ya existe",
+    "Esiste già un utente con lo stesso indirizzo email" => "Existe ya un usuario con la mismo dirección email",
+    "Esiste già un utente con lo stesso codice fiscale" => "Existe ya un usuario con el mismo numero de identificación",
+    "E' necessario essere maggiorenni" => "Es necesario ser mayor de edad",
+    "E' necessario avere la patente da almeno un anno" => "Es’ necesario tener la licencia de almenos un año",
+    "La partita IVA non può contenere spazi" => "La partida IVA non puede contener espacios",
+    "La partita IVA deve essere numerica, al più preceduta dal prefisso IT" => "La partida IVA debe ser numerica, o seguida del prefijo IT",
+    "La partita IVA deve essere di 11 cifre, al più preceduta dal prefisso IT" => "La partita IVA debe ser de 11 cifras, o seguida del  prefijo IT"
 ];
