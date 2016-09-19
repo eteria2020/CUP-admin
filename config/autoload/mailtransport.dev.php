33cd054bd5bcaf6877c26c7daee715024fa7d73f
Andrea Cadorin <cadorin89@gmail.com>
2016-09-19 13:24:05 +0200

    Revert to previous mailtransport config

--- config/autoload/mailtransport.dev.php
+++ config/autoload/mailtransport.dev.php
@@ -5,8 +5,8 @@ use Zend\Mail\Transport\File;
 return [
     'emailTransport' => [
         'type' => 'file',
-        'transport_options' => [
-            'path' => realpath(__DIR__ . "/../../data/mails/"),
+        'options' => [
+            'path' => realpath(__DIR__ . "/../../data/mails"),
             'callback' => function (File $transport) {
                 return 'Message_' . microtime(true) . '_' . mt_rand() . '.txt';
             }
