f09a535f6371d73a8320872508a9050d6e852b34
Enrico Taddei <cup@sharengo.eu>
2016-10-05 14:51:13 +0200

    superadmin-161005:only super admin show user area on combo-box top right

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1658,6 +1658,10 @@ return [
 
                     // for limiting certains operations
                     [['superadmin','admin'], 'customer', 'changeEmail'],
+                    [['superadmin'], 'customer', 'userArea'],
+                    [['superadmin'], 'customer', 'discountRate'],
+                    [['superadmin'], 'customer', 'maintainer'],
+                    [['superadmin'], 'customer', 'goldList'],
                 ],
             ],
         ],
