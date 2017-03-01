d9fa5423a29baf6cd1e2c85260dfa202e969f231
Enrico Taddei <cup@sharengo.eu>
2017-03-01 15:40:06 +0100

    trip-cost-170228-enable-menu:update trip cost

--- module/Application/src/Application/Form/TripCostFieldset.php
+++ module/Application/src/Application/Form/TripCostFieldset.php
@@ -45,27 +45,13 @@ class TripCostFieldset extends Fieldset
         ]);
 
         $this->add([
-            'name' => 'tripParkSeconds',
+            'name' => 'tripParkMinutes',
             'type' => 'Zend\Form\Element\Number',
             'attributes' => [
-                'id' => 'trip-park-seconds',
+                'id' => 'trip-park-minutes',
                 'class' => 'form-control',
-                'min' => 0
-            ]
-        ]);
-
-        $this->add([
-            'name' => 'customerGender',
-            'type' => 'Zend\Form\Element\Select',
-            'attributes' => [
-                'id' => 'customer-gender',
-                'class' => 'form-control'
-            ],
-            'options' => [
-                'value_options' => [
-                    'male' => $translator->translate('Maschio'),
-                    'female' => $translator->translate('Femmina')
-                ]
+                'min' => 0,
+                'value' => 0
             ]
         ]);
 
@@ -75,7 +61,8 @@ class TripCostFieldset extends Fieldset
             'attributes' => [
                 'id' => 'customer-bonus',
                 'class' => 'form-control',
-                'min' => 0
+                'min' => 0,
+                'value' => 0
             ]
         ]);
     }
