59cd7cbef4ca7b770e774f8c9b6a561f7e76c78d
Enrico Taddei <tommaso.garavaglia@cdesign-group.com>
2017-12-13 14:44:07 +0100

    trips-171121-maptrip: update 13/12/17 14.45

--- public/js/mapTrips.js
+++ public/js/mapTrips.js
@@ -1,32 +1,32 @@
-var url_image_DEFAULT = "/img/DEFAULT.PNG";
-var url_image_SW_BOOT = "/img/SW_BOOT.PNG";
-var url_image_RFID = "/img/RFID.PNG";
-var url_image_BATTERY = "/img/BATTERY.PNG";
-var url_image_SPEED = "/img/SPEED.PNG";
-var url_image_AREA = "/img/AREA.PNG";
-var url_image_CHARGING = "/img/CHARGING.PNG";
-var url_image_ENGINE = "/img/ENGINE.PNG";
-var url_image_SOS = "/img/SOS.PNG";
-var url_image_PARK = "/img/PARK.PNG";
-var url_image_CMD = "/img/CMD.PNG";
-var url_image_CLEANLINESS = "/img/CLEANLINESS.PNG";
-var url_image_OBCFAIL = "/img/OBCFAIL.PNG";
-var url_image_OBCOK = "/img/OBCOK.PNG";
-var url_image_KEY = "/img/KEY.PNG";
-var url_image_READY = "/img/READY.PNG";
-var url_image_GEAR = "/img/GEAR.PNG";
-var url_image_DIAG = "/img/DIAG.PNG";
-var url_image_CARPLATE = "/img/CARPLATE.PNG";
-var url_image_3G = "/img/3G.PNG";
-var url_image_MAINTENANCE = "/img/MAINTENANCE.PNG";
-var url_image_OUTOFORDER = "/img/OUTOFORDER.PNG";
-var url_image_SELFCLOSE = "/img/SELFCLOSE.PNG";
-var url_image_DEVICEINFO = "/img/DEVICEINFO.PNG";
-var url_image_SHUTDOWN = "/img/SHUTDOWN.PNG";
-var url_image_LEASE = "/img/LEASE.PNG";
-var url_image_SOC = "/img/SOC.PNG";
-//var url_image_AREA = "/img/.PNG";
-var url_image_MENU_CLICK = "/img/MENU_CLICK.PNG";
+var img_DEFAULT = "/img/DEFAULT.PNG";
+var img_SW_BOOT = "/img/SW_BOOT.PNG";
+var img_RFID = "/img/RFID.PNG";
+var img_BATTERY = "/img/BATTERY.PNG";
+var img_SPEED = "/img/SPEED.PNG";
+var img_AREA = "/img/AREA.PNG";
+var img_CHARGING = "/img/CHARGING.PNG";
+var img_ENGINE = "/img/ENGINE.PNG";
+var img_SOS = "/img/SOS.PNG";
+var img_PARK = "/img/PARK.PNG";
+var img_CMD = "/img/CMD.PNG";
+var img_CLEANLINESS = "/img/CLEANLINESS.PNG";
+var img_OBCFAIL = "/img/OBCFAIL.PNG";
+var img_OBCOK = "/img/OBCOK.PNG";
+var img_KEY = "/img/KEY.PNG";
+var img_READY = "/img/READY.PNG";
+var img_GEAR = "/img/GEAR.PNG";
+var img_DIAG = "/img/DIAG.PNG";
+var img_CARPLATE = "/img/CARPLATE.PNG";
+var img_3G = "/img/3G.PNG";
+var img_MAINTENANCE = "/img/MAINTENANCE.PNG";
+var img_OUTOFORDER = "/img/OUTOFORDER.PNG";
+var img_SELFCLOSE = "/img/SELFCLOSE.PNG";
+var img_DEVICEINFO = "/img/DEVICEINFO.PNG";
+var img_SHUTDOWN = "/img/SHUTDOWN.PNG";
+var img_LEASE = "/img/LEASE.PNG";
+var img_SOC = "/img/SOC.PNG";
+//var img_AREA = "/img/.PNG";
+var img_MENU_CLICK = "/img/MENU_CLICK.PNG";
 
 var zoom = 14;
 var popup = null;
@@ -58,93 +58,93 @@ var init = function (events) {
 
         switch (event['eventType']) {
             case 'SW_BOOT':
-                var icon = setIcon(url_image_SW_BOOT, size, offset);
+                var icon = setIcon(img_SW_BOOT, size, offset);
                 break;
             case 'RFID':
-                var icon = setIcon(url_image_RFID, size, offset);
+                var icon = setIcon(img_RFID, size, offset);
                 break;
             case 'BATTERY':
-                var icon = setIcon(url_image_BATTERY, size, offset);
+                var icon = setIcon(img_img_BATTERY, size, offset);
                 break;
             case 'SPEED':
-                var icon = setIcon(url_image_SPEED, size, offset);
+                var icon = setIcon(img_img_SPEED, size, offset);
                 break;
             case 'AREA':
-                var icon = setIcon(url_image_AREA, size, offset);
+                var icon = setIcon(img_AREA, size, offset);
                 break;
             case 'CHARGING':
-                var icon = setIcon(url_image_CHARGING, size, offset);
+                var icon = setIcon(img_CHARGING, size, offset);
                 break;
             case 'ENGINE':
-                var icon = setIcon(url_image_ENGINE, size, offset);
+                var icon = setIcon(img_ENGINE, size, offset);
                 break;
             case 'SOS':
-                var icon = setIcon(url_image_SOS, size, offset);
+                var icon = setIcon(img_SOS, size, offset);
                 break;
             case 'PARK':
-                var icon = setIcon(url_image_PARK, size, offset);
+                var icon = setIcon(img_PARK, size, offset);
                 break;
             case 'CMD':
-                var icon = setIcon(url_image_CMD, size, offset);
+                var icon = setIcon(img_CMD, size, offset);
                 break;
             case 'CLEANLINESS':
-                var icon = setIcon(url_image_CLEANLINESS, size, offset);
+                var icon = setIcon(img_CLEANLINESS, size, offset);
                 break;
             case 'OBCFAIL':
-                var icon = setIcon(url_image_OBCFAIL, size, offset);
+                var icon = setIcon(img_OBCFAIL, size, offset);
                 break;
             case 'OBCOK':
-                var icon = setIcon(url_image_OBCOK, size, offset);
+                var icon = setIcon(img_OBCOK, size, offset);
                 break;
             case 'KEY':
-                var icon = setIcon(url_image_KEY, size, offset);
+                var icon = setIcon(img_KEY, size, offset);
                 break;
             case 'READY':
-                var icon = setIcon(url_image_READY, size, offset);
+                var icon = setIcon(img_READY, size, offset);
                 break;
             case 'GEAR':
-                var icon = setIcon(url_image_GEAR, size, offset);
+                var icon = setIcon(img_GEAR, size, offset);
                 break;
             case 'DIAG':
-                var icon = setIcon(url_image_DIAG, size, offset);
+                var icon = setIcon(img_DIAG, size, offset);
                 break;
             case 'CARPLATE':
-                var icon = setIcon(url_image_CARPLATE, size, offset);
+                var icon = setIcon(img_CARPLATE, size, offset);
                 break;
             case '3G':
-                var icon = setIcon(url_image_3G, size, offset);
+                var icon = setIcon(img_3G, size, offset);
                 break;
             case 'MAINTENANCE':
-                var icon = setIcon(url_image_MAINTENANCE, size, offset);
+                var icon = setIcon(img_MAINTENANCE, size, offset);
                 break;
             case 'OUTOFORDER':
-                var icon = setIcon(url_image_OUTOFORDER, size, offset);
+                var icon = setIcon(img_OUTOFORDER, size, offset);
                 break;
             case 'SELFCLOSE':
-                var icon = setIcon(url_image_SELFCLOSE, size, offset);
+                var icon = setIcon(img_SELFCLOSE, size, offset);
                 break;
             case 'DEVICEINFO':
-                var icon = setIcon(url_image_DEVICEINFO, size, offset);
+                var icon = setIcon(img_DEVICEINFO, size, offset);
                 break;
             case 'SHUTDOWN':
-                var icon = setIcon(url_image_SHUTDOWN, size, offset);
+                var icon = setIcon(img_SHUTDOWN, size, offset);
                 break;
             case 'LEASE':
-                var icon = setIcon(url_image_LEASE, size, offset);
+                var icon = setIcon(img_LEASE, size, offset);
                 break;
             case 'SOC':
-                var icon = setIcon(url_image_SOC, size, offset);
+                var icon = setIcon(img_SOC, size, offset);
                 break;
                 /*
                  case 'AREA':
-                 var icon = setIcon(url_image_AREA, size, offset);
+                 var icon = setIcon(img_AREA, size, offset);
                  break;
                  */
             case 'MENU_CLICK':
-                var icon = setIcon(url_image_MENU_CLICK, size, offset);
+                var icon = setIcon(img_MENU_CLICK, size, offset);
                 break;
             default:
-                var icon = setIcon(url_image_DEFAULT, size, offset);
+                var icon = setIcon(img_DEFAULT, size, offset);
                 break;
         }
 
