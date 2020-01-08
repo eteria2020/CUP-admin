10df476b740c6fd249bac0141e95b4195476bf37
Enrico Taddei <enrico.taddei@sharengo.eu>
2020-01-08 14:28:51 +0100

    Nl 200108 reverse geocode

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Require
 - [Zend Framework 2.4](https://framework.zend.com/downloads/archives) 
 - [Postgre SQL 9.4.5](https://www.postgresql.org/docs/9.4/index.html)
 - [Doctrine 0.9](https://www.doctrine-project.org/2015/05/05/doctrine-orm-module-release-0-9-0.html)
+- Google API
+- [Sharengo Reverse Geocoding](https://maps.sharengo.it/reverse.php?format=json&zoom=18&addressdetails=1&lon=4.471896&lat=51.935639)
--- module/Alarms/public/assets-modules/alarms/js/directives/angular-reverse-geocode.js
+++ module/Alarms/public/assets-modules/alarms/js/directives/angular-reverse-geocode.js
@@ -1,5 +1,5 @@
 angular.module('SharengoCsApp')
-    .directive('reverseGeocode', function (uiGmapIsReady) {
+    .directive('reverseGeocode', function (uiGmapIsReady, $http) {
         return {
             restrict: 'E',
             scope: {
@@ -8,23 +8,17 @@ angular.module('SharengoCsApp')
             },
             link: function (scope, element, attrs) {
                 uiGmapIsReady.promise().then(function () {
-                    var geocoder = new google.maps.Geocoder();
+                    element.text('Indirizzo non trovato');
                     scope.$watch('lng', function () {
                         if (scope.lat !== undefined && scope.lng !== undefined) {
-                            var latlng = new google.maps.LatLng(scope.lat, scope.lng);
-                            geocoder.geocode({
-                                'latLng': latlng,
-                                'language': 'it'
-                            }, function (results, status) {
-                                if (status === google.maps.GeocoderStatus.OK) {
-                                    if (results[1]) {
-                                        element.text(results[0].formatted_address);
-                                    } else {
-                                        element.text('Indirizzo non trovato');
-                                    }
-                                } else {
-                                    element.text('Indirizzo non trovato');
-                                }
+                            $http({
+                                method: 'GET',
+                                url: 'https://maps.sharengo.it/reverse.php?format=json&zoom=18&addressdetails=1&lon=' + scope.lng + '&lat=' + scope.lat
+                            }).then(function successCallback(response) {
+                                console.log(response);
+                                element.text(response.data.display_name);
+                            }, function errorCallback(response) {
+
                             });
                         }
                     });
